31d651da35e445374234df1a6c77ff6f52918eff
Ali400up <127770993+Ali400up@users.noreply.github.com>
2026-08-15 15:28:29 +0300

    Delete supabase/functions/frist_admin.sql

--- supabase/functions/frist_admin.sql
+++
@@ -1,8 +0,0 @@
-UPDATE public.profiles
-SET
-    phone = '779512515',
-    role = 'admin'::public.app_role,
-    status = 'active',
-    is_active = true,
-    expires_at = NULL
-WHERE email = 'u779512515@zakat.local';
